edb7b881288d52e97482470f43d73e8377d53590
Ahmed Noweir <ge59juj@mytum.de>
2023-06-07 17:58:44 +0200

    d

--- public/frontend.js
+++ public/frontend.js
@@ -1,4 +1,8 @@
-// Function to handle the file upload
+let globalDiffFile = '';
+let oldTreeText = '';
+let newTreeText = '';
+let cache = '<cache></cache>'
+
 function uploadTrees() {
     const fileInput1 = document.getElementById('fileInput1');
     const fileInput2 = document.getElementById('fileInput2');
@@ -13,14 +17,16 @@ function uploadTrees() {
     const reader1 = new FileReader();
     const reader2 = new FileReader();
 
-    reader1.onload = function(event) {
+    reader1.onload = function (event) {
         const content1 = event.target.result;
         reader2.readAsText(files2[0]);
-        reader2.onload = function(event) {
+        reader2.onload = function (event) {
             const content2 = event.target.result;
 
             // Combine the contents with '&'
             const combinedContent = content1.trim() + '&' + content2.trim();
+            oldTreeText = content1;
+            newTreeText = content2;
 
             // Send the combined content to the backend server
             sendTreesToServer(combinedContent);
@@ -33,7 +39,7 @@ function uploadTrees() {
 // Function to send the process trees to the backend server
 function sendTreesToServer(content) {
     const url = new URL("http://localhost:3000")
-    fetch(url + 'diff', {
+    fetch('/cpeediff/diff', {
         headers: {
             'Content-Type': 'text/xml'
         },
@@ -42,6 +48,7 @@ function sendTreesToServer(content) {
     })
         .then(response => response.text())
         .then(diffFile => {
+            globalDiffFile = diffFile;
             // Display the diff file
             displayDiffFile(diffFile);
         })
@@ -57,62 +64,175 @@ function displayDiffFile(diffFile) {
     diffOutput.textContent = diffFile;
 }
 
-function displayOldTree() {
-    const fileInput = document.getElementById('fileInput1');
-    const file = fileInput.files[0];
-
-    if (!file) {
-        alert('Please select an old XML file');
-        console.error('No file selected.');
-        return;
-    }
-
-    const reader = new FileReader();
-    reader.onload = function(event) {
-        const content = event.target.result;
-        displayTree(content, '#oldTree', '#graphcanvas1')
-        // const oldTreeOutput = document.getElementById('oldTree');
-        // oldTreeOutput.textContent = content;
-    }
-    reader.onerror = function(event) {
-        console.error('Error reading the file:', event.target.error);
-    };
-
-    reader.readAsText(file);
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
 }
 
-function displayNewTree() {
-    const fileInput = document.getElementById('fileInput2');
-    const file = fileInput.files[0];
-
-    if (!file) {
-        alert('Please select a new XML file');
-        console.error('No file selected.');
-        return;
-    }
-
-    const reader = new FileReader();
-    reader.onload = function(event) {
-        const content = event.target.result;
-        displayTree(content, '#newTree', '#graphcanvas2')
-        // const oldTreeOutput = document.getElementById('oldTree');
-        // oldTreeOutput.textContent = content;
-    }
-    reader.onerror = function(event) {
-        console.error('Error reading the file:', event.target.error);
-    };
-
-    reader.readAsText(file);
+function displayTrees() {
+    uploadTrees();
+    delay(500).then(() => {
+        displayBothTrees(oldTreeText, '#oldTree', '#graphcanvas1', newTreeText, '#newTree', '#graphcanvas2')
+    });
 }
-
-function displayTree(treeXML, divId, svgId) {
-    var parser, xmlDoc;
+function displayBothTrees(oldTreeXML, oldDivId, oldSvgId, newTreeXML, newDivId, newSvgId) {
+    var parser, oldXmlDoc, newXmlDoc, cache;
     parser = new DOMParser();
-    xmlDoc = parser.parseFromString(treeXML,"text/xml");
-    let graphrealization = new WfAdaptor('http://127.0.0.1:8062/cockpit/themes/extended/theme.js',function(graphrealization){
+    oldXmlDoc = parser.parseFromString(oldTreeXML, "text/xml");
+    newXmlDoc = parser.parseFromString(newTreeXML, "text/xml");
+    cache = parser.parseFromString(cache, "text/xml");
+
+
+    const diffDoc = parser.parseFromString(globalDiffFile, "text/xml");
+    const diffElements = diffDoc.getElementsByTagName("*");
+    let indexAdjustmentForNewTree = [0];
+    for (let i = 0; i < diffElements.length; i++) {
+        const diffElement = diffElements[i];
+        if (diffElement.tagName === "insert") {
+            const newPath = diffElement.getAttribute("newPath");
+
+            const elementId = diffElement.childNodes[1].getAttribute("id");
+
+            if (newPath) {
+                const newPathArray = newPath.split("/").map(Number);
+
+                const emptyElement = cache.createElement("empty");
+                const textElement = cache.createTextNode("\n    ");
+
+                let currentNode = oldXmlDoc.getRootNode().childNodes[0];
+                for (let j = 1; j < newPathArray.length; j++) {
+                    if (currentNode.childNodes.length <= 2 * newPathArray[j] + 1) {
+                        const textElement = cache.createTextNode("\n    ");
+                        const commentElement = cache.createComment("Placeholder");
+                        currentNode.appendChild(commentElement);
+                        currentNode.appendChild(textElement)
+                    }
+                    currentNode = currentNode.childNodes[2 * newPathArray[j] + 1];
+                }
+
+                emptyElement.setAttribute('id', elementId);
+
+                currentNode.parentNode.insertBefore(emptyElement, currentNode);
+                currentNode.parentNode.insertBefore(textElement, currentNode);
+            }
+        }
+        if (diffElement.tagName === "delete") {
+            const oldPath = diffElement.getAttribute("oldPath");
+            let depth = 0;
+            if (oldPath) {
+                const oldPathArray = oldPath.split("/").map(Number);
+
+                const emptyElement = cache.createElement("empty");
+                const textElement = cache.createTextNode("\n    ");
+
+                let originNode = oldXmlDoc.getRootNode().childNodes[0];
+                let currentNode = newXmlDoc.getRootNode().childNodes[0];
+
+                for (let j = 1; j < oldPathArray.length; j++) {
+                    if (!indexAdjustmentForNewTree[depth + 1]) {
+                        indexAdjustmentForNewTree[depth + 1] = 0;
+                    }
+                    if (currentNode.childNodes.length <= indexAdjustmentForNewTree[depth + 1] + 2 * oldPathArray[j] + 1) {
+                        const textElement = cache.createTextNode("\n    ");
+                        const commentElement = cache.createComment("Placeholder");
+                        currentNode.appendChild(commentElement);
+                        currentNode.appendChild(textElement)
+                    }
+                    currentNode = currentNode.childNodes[indexAdjustmentForNewTree[depth + 1] + 2 * oldPathArray[j] + 1];
+
+                    if (originNode.childNodes.length <= indexAdjustmentForNewTree[depth + 1] + 2 * oldPathArray[j] + 1) {
+                        const textElement = cache.createTextNode("\n    ");
+                        const commentElement = cache.createComment("Placeholder");
+                        originNode.appendChild(commentElement);
+                        originNode.appendChild(textElement)
+                    }
+                    originNode = originNode.childNodes[indexAdjustmentForNewTree[depth + 1] + 2 * oldPathArray[j] + 1];
+
+                    depth += 1;
+                }
+                emptyElement.setAttribute("id", originNode.getAttribute("id"));
+                currentNode.parentNode.insertBefore(emptyElement, currentNode);
+                currentNode.parentNode.insertBefore(textElement, currentNode);
+                indexAdjustmentForNewTree[depth] += 2;
+
+            }
+        }
+        // TODO: Add logic for other diffElements here
+    }
+
+    displayOneTree(oldDivId, oldSvgId, oldXmlDoc)
+
+
+    delay(1000).then(() => {
+        displayOneTree(newDivId, newSvgId, newXmlDoc)
+    });
+
+    // Color the trees here
+}
+
+function displayOneTree(divId, svgId, xmlDoc){
+    let graphrealization = new WfAdaptor('http://localhost/cockpit/themes/extended/theme.js', function (graphrealization) {
+
+        graphrealization.draw_labels = function (max, labels, shift, striped) {
+            $(svgId).css('grid-row', '1/span ' + (max.row + 2));
+            if (striped == true) {
+                if (!$(divId).hasClass('striped')) {
+                    $(divId).addClass('striped');
+                }
+            } else {
+                $(divId).removeClass('striped');
+            }
+
+            $(svgId + '.graphlabel, ' + divId + ' .graphempty, ' + divId + ' .graphlast').remove();
+            var tlabels = {};
+            var tcolumns = [];
+            var tcolumncount = {}
+            _.each(labels, function (val) {
+                if (val.label != "") {
+                    tlabels[val.row] = [];
+                    _.each(val.label, function (col) {
+                        if (!tcolumns.includes(col.column)) {
+                            tcolumns.push(col.column);
+                            tcolumncount[col.column] = 0;
+                        }
+                        if (col.value != undefined) {
+                            tcolumncount[col.column] += 1;
+                        }
+                        tlabels[val.row][tcolumns.indexOf(col.column)] = {
+                            label: col.value,
+                            type: val.tname,
+                            id: val.element_id
+                        };
+                    });
+                }
+            });
+            $(divId).css({
+                'grid-template-rows': (shift / 2) + 'px repeat(' + max.row + ', 1fr) ' + (shift / 2) + 'px',
+                'grid-template-columns': 'max-content' + (tcolumns.length > 0 ? ' repeat(' + tcolumns.length.toString() + ',max-content)' : '') + ' auto'
+            });
+            for (var i = 0; i < max.row; i++) {
+                for (var j = 0; j < tcolumns.length; j++) {
+                    if (tlabels[i + 1] != undefined && tlabels[i + 1][j] != undefined && tlabels[i + 1][j].label != undefined && tlabels[i + 1][j].label != '') {
+                        var col = tlabels[i + 1][j];
+                        var ele = $('<div class="graphlabel ' + (i % 2 == 0 ? 'odd' : 'even') + '" element-type="' + col.type + '" element-id="' + col.id + '" style="grid-column: ' + (j + 2) + '; grid-row: ' + (i + 2) + '"><span>' + col.label + '</span></div>');
+                        graphrealization.illustrator.draw.bind_event(ele, col.type, false);
+                        $(divId).append(ele);
+                    } else {
+                        if (tcolumncount[tcolumns[j]] != 0) {
+                            var ele = $('<div class="graphempty ' + (i % 2 == 0 ? 'odd' : 'even') + '" style="grid-column: ' + (j + 2) + '; grid-row: ' + (i + 2) + '; padding-bottom: ' + shift + 'px">&#032;</div>');
+                            $(divId).append(ele);
+                        }
+                    }
+                }
+                var j = tcolumns.length;
+                var ele = $('<div class="graphlast ' + (i % 2 == 0 ? 'odd' : 'even') + '" style="grid-column: ' + (j + 2) + '; grid-row: ' + (i + 2) + '; padding-bottom: ' + shift + 'px">&#032;</div>');
+                $(divId).append(ele);
+            }
+            $(divId).find('[element-id="reserve1"]').css('background-color', 'green');
+
+        };
         graphrealization.set_svg_container($(svgId));
         graphrealization.set_label_container($(divId));
         graphrealization.set_description($(xmlDoc), true);
     });
 }
-// https://cpee.org/flow/themes/extended/theme.js
+// style="fill:green;" for <recT>
